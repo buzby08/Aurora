0042888ba956b1ba17cc1ad33bf0adcd1ce6805e
LiamBusby <lbusby685@gmail.com>
2026-03-19 19:22:33 +0000

    Updated version

--- Aurora/Properties/AssemblyInfo.cs
+++ Aurora/Properties/AssemblyInfo.cs
@@ -3,10 +3,11 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Aurora.Tests")]
 [assembly: AssemblyTitle("Aurora")]
-[assembly: AssemblyDescription("A readable, modern programming language built for simplicity, power, and expressiveness.")]
+[assembly:
+    AssemblyDescription("A readable, modern programming language built for simplicity, power, and expressiveness.")]
 [assembly: AssemblyCompany("Liam Busby")]
 [assembly: AssemblyProduct("Aurora")]
 [assembly: AssemblyCopyright("© 2025 Liam Busby")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
