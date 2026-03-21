78bfe0dd51669d4459a45e05897125b2ea84affb
LiamBusby <lbusby685@gmail.com>
2026-03-21 19:35:04 +0000

    Added new type & methods, used variadic arguments

--- Aurora/Internals/Builtins.cs
+++ Aurora/Internals/Builtins.cs
@@ -3,7 +3,7 @@ using Aurora.BuiltinMethods;
 
 namespace Aurora.Internals;
 
-internal class Builtins
+internal static class Builtins
 {
     public static Type Type;
     public static Type Int;
@@ -30,7 +30,7 @@ internal class Builtins
         InitialiseIntType();
 
         Float = new Type("Float", type: Type);
-        
+
         InitialiseFloatType();
 
         String = new Type("String", type: Type);
@@ -39,7 +39,11 @@ internal class Builtins
 
         Boolean = new Type("Boolean", type: Type);
 
+        InitialiseBooleanType();
+
         Null = new Type("Null", type: Type);
+
+        InitialiseNullType();
 
         Terminal = new Type("Terminal", type: Type);
 
@@ -98,6 +102,15 @@ internal class Builtins
             });
 
         Type.AddStaticMethod(typeSetMethod);
+
+        Method toString = new(
+            name: "toString",
+            returnType: String,
+            parameters: [],
+            body: (self, args, context) => new StringObject($"{self.Type.Name}"));
+
+        Type.AddInstanceMethod(toString);
+        Type.AddStaticMethod(toString);
     }
 
     public static void InitialiseIntType()
@@ -116,7 +129,7 @@ internal class Builtins
             });
 
         Int.AddInstanceMethod(addMethod);
-        
+
         Method subtractMethod = new(
             name: "subtract",
             returnType: Int,
@@ -129,9 +142,9 @@ internal class Builtins
                 return new IntObject(
                     left.Value - right.Value);
             });
-        
+
         Int.AddInstanceMethod(subtractMethod);
-        
+
         Method multiplyByMethod = new(
             name: "multiplyBy",
             returnType: Int,
@@ -144,9 +157,9 @@ internal class Builtins
                 return new IntObject(
                     left.Value * right.Value);
             });
-        
+
         Int.AddInstanceMethod(multiplyByMethod);
-        
+
         Method divideByMethod = new(
             name: "divideBy",
             returnType: Float,
@@ -159,7 +172,7 @@ internal class Builtins
                 return new FloatObject(
                     (decimal)left.Value / right.Value);
             });
-        
+
         Int.AddInstanceMethod(divideByMethod);
 
         Method toString = new(
@@ -197,6 +210,62 @@ internal class Builtins
 
         String.AddInstanceMethod(stringAddMethod);
 
+        Method toString = new(
+            name: "toString",
+            returnType: String,
+            parameters: [],
+            body: (self, args, context) =>
+            {
+                StringObject selfAsString = (StringObject)self;
+
+                return selfAsString;
+            });
+
+        String.AddInstanceMethod(toString);
+
+        Method staticConcatMethod = new(
+            name: "concat",
+            returnType: String,
+            unlimitedPositionalArgumentsType: Type,
+            unlimitedKeywordArgumentsType: null,
+            body: (self, args, context) =>
+            {
+                // Todo: Change how positional args stored in context (naming)
+                // Todo: Get all positional args from context, and add to full string
+                string fullString = string.Empty;
+
+                foreach ((string key, List<Ast> value) in args)
+                {
+                    RuntimeObject valueAsObject = Evaluator.EvaluateAstList(value, context.Parent!);
+                    StringObject valueAsStringObject = valueAsObject.ConvertToStringObject(context);
+
+                    if (fullString != string.Empty)
+                        fullString += ' ';
+
+                    fullString += valueAsStringObject.Value;
+                }
+
+                return new StringObject(fullString);
+            });
+
+        String.AddStaticMethod(staticConcatMethod);
+
+        Method instanceConcatMethod = new(
+            name: "concat",
+            returnType: String,
+            parameters: [new ParameterDefinition(name: "other", type: Type)],
+            body: (self, args, context) =>
+            {
+                StringObject left = (StringObject)self;
+
+                RuntimeObject right = context.Get("other");
+                StringObject rightAsStringObject = right.ConvertToStringObject(context);
+
+                return new StringObject(left.Value + rightAsStringObject.Value);
+            });
+
+        String.AddInstanceMethod(instanceConcatMethod);
+
         // Todo: Add other StringType methods
     }
 
@@ -205,22 +274,114 @@ internal class Builtins
         Method writeMethod = new(
             name: "writeLine",
             returnType: Unit,
+            unlimitedPositionalArgumentsType: Type,
             parameters:
             [
-                new ParameterDefinition(name: "value", type: String),
+                // Todo: Make use of new unlimitedPositionalArgs
+                // Todo: Change all SystemError calls to have a unique identifier, to find their location in the code.
                 new ParameterDefinition(name: "end", type: String, defaultValue: new StringObject("\n"))
             ],
+            unlimitedKeywordArgumentsType: null,
             body: (self, args, context) =>
             {
-                StringObject value = (StringObject)context.GetParam("value");
+                List<RuntimeObject> values = context.GetPositionalArgs();
+                string valueToOutput = string.Empty;
+
+                foreach (RuntimeObject value in values)
+                {
+                    valueToOutput += value.ConvertToCSharpString(context);
+                }
+
                 StringObject defaultParam = (StringObject)context.GetParam("end");
 
-                Console.Write(value.Value + defaultParam.Value);
+                Console.Write(valueToOutput + defaultParam.Value);
 
                 return new UnitObject();
             });
 
         Terminal.AddStaticMethod(writeMethod);
+
+        Method readMethod = new(
+            name: "readLine",
+            returnType: String,
+            parameters:
+            [
+                new ParameterDefinition(name: "message", type: String, defaultValue: new StringObject("")),
+                new ParameterDefinition(name: "default", type: String, nullable: true, defaultValue: new NullObject())
+            ],
+            body: (self, args, context) =>
+            {
+                StringObject message = (StringObject)context.GetParam("message");
+                RuntimeObject defaultValueObject = context.GetParam("default");
+
+                Console.Write(message.Value);
+                string? inputtedValue = Console.ReadLine();
+
+                if (defaultValueObject is not NullObject && string.IsNullOrWhiteSpace(inputtedValue))
+                    return (StringObject)defaultValueObject;
+
+                return new StringObject(inputtedValue ?? string.Empty);
+            });
+
+        Terminal.AddStaticMethod(readMethod);
+
+        Method readIntMethod = new(
+            name: "readInt",
+            returnType: Int,
+            parameters:
+            [
+                new ParameterDefinition(name: "message", type: String, defaultValue: new StringObject("")),
+                new ParameterDefinition(name: "min", type: Int, nullable: true, defaultValue: new NullObject()),
+                new ParameterDefinition(name: "max", type: Int, nullable: true, defaultValue: new NullObject())
+            ],
+            body: (self, args, context) =>
+            {
+                StringObject message = (StringObject)context.GetParam("message");
+                RuntimeObject minObject = context.GetParam("min");
+                RuntimeObject maxObject = context.GetParam("max");
+
+                int? minValue = minObject is NullObject ? null : ((IntObject)minObject!).Value;
+                int? maxValue = maxObject is NullObject ? null : ((IntObject)maxObject!).Value;
+
+                while (true)
+                {
+                    Console.Write(message.Value);
+                    string? inputtedValue = Console.ReadLine();
+                    bool isAnInt = int.TryParse(inputtedValue, out int inputtedInt);
+                    bool satisfiesMinRequirement = minValue is null || inputtedInt >= minValue;
+                    bool satisfiesMaxRequirement = maxValue is null || inputtedInt <= maxValue;
+
+                    if (isAnInt && satisfiesMaxRequirement && satisfiesMinRequirement)
+                        return new IntObject(inputtedInt);
+
+                    if (!isAnInt)
+                    {
+                        Console.WriteLine("Please input an integer value");
+                        continue;
+                    }
+
+                    if (!satisfiesMaxRequirement && !satisfiesMinRequirement)
+                    {
+                        Console.WriteLine(
+                            $"Please input a value greater than or equal to {minValue} and less than or equal to {maxValue}");
+                        continue;
+                    }
+
+                    if (!satisfiesMaxRequirement)
+                    {
+                        Console.WriteLine($"Please enter a value less than or equal to {maxValue}");
+                        continue;
+                    }
+
+                    if (!satisfiesMinRequirement)
+                    {
+                        Console.WriteLine($"Please enter a value greater than or equal to {minValue}");
+                        continue;
+                    }
+                }
+            });
+
+        Terminal.AddStaticMethod(readIntMethod);
 
         // Todo: Add other TerminalType methods
     }
@@ -236,7 +397,7 @@ internal class Builtins
                 FloatObject selfAsFloat = (FloatObject)self;
 
                 string valueAsString = selfAsFloat.Value.ToString(CultureInfo.InvariantCulture);
-                
+
                 if (valueAsString.EndsWith(".0"))
                     valueAsString = valueAsString[..^2];
 
@@ -246,7 +407,34 @@ internal class Builtins
         Float.AddInstanceMethod(toString);
     }
 
-    // Todo: Add BooleanType methods
+    public static void InitialiseBooleanType()
+    {
+        Method toString = new(
+            name: "toString",
+            returnType: String,
+            parameters: [],
+            body: (self, args, context) =>
+            {
+                BooleanObject selfAsBoolean = (BooleanObject)self;
 
-    // Todo: Add NullType methods
+                return new StringObject(selfAsBoolean.Value ? "true" : "false");
+            });
+
+        Boolean.AddInstanceMethod(toString);
+
+        // Todo: Add more BooleanType methods
+    }
+
+    public static void InitialiseNullType()
+    {
+        Method toString = new(
+            name: "toString",
+            returnType: String,
+            parameters: [],
+            body: (self, args, context) => new StringObject("null"));
+
+        Null.AddInstanceMethod(toString);
+
+        // Todo: Add more NullType methods
+    }
 }
