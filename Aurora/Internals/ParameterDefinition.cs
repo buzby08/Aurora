61d6981c244308789437197c6f826fcf9ea75f43
LiamBusby <lbusby685@gmail.com>
2026-03-21 19:32:16 +0000

    Added nullable flag

--- Aurora/Internals/ParameterDefinition.cs
+++ Aurora/Internals/ParameterDefinition.cs
@@ -1,8 +1,9 @@
 namespace Aurora.Internals;
 
-internal class ParameterDefinition(string name, Type type, RuntimeObject? defaultValue = null)
+internal class ParameterDefinition(string name, Type type, bool nullable = false, RuntimeObject? defaultValue = null)
 {
     public string Name { get; } = name;
     public Type Type { get; } = type;
+    public bool Nullable { get; } = false;
     public RuntimeObject? DefaultValue { get; } = defaultValue;
 }
