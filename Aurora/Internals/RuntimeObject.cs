9c3879833ff4dfc2c4374933c7c66a81d857a896
LiamBusby <lbusby685@gmail.com>
2026-03-21 19:30:44 +0000

    Added toString helpers

--- Aurora/Internals/RuntimeObject.cs
+++ Aurora/Internals/RuntimeObject.cs
@@ -6,7 +6,25 @@ internal class RuntimeObject
 {
     public Type Type;
 
-    public static RuntimeObject CreateFromToken(Token token, RuntimeContext context)
+    public StringObject ConvertToStringObject(RuntimeContext context)
+    {
+        RuntimeObject evaluatedValueAsObject =
+            this.Type.GetInstanceMethod("toString")
+                .Invoke(this, [], new RuntimeContext(context));
+        StringObject valueAsString = (StringObject)evaluatedValueAsObject;
+        return valueAsString;
+    }
+    
+    public string ConvertToCSharpString(RuntimeContext context)
+    {
+        RuntimeObject evaluatedValueAsObject =
+            this.Type.GetInstanceMethod("toString")
+                .Invoke(this, [], new RuntimeContext(context));
+        StringObject valueAsString = (StringObject)evaluatedValueAsObject;
+        return valueAsString.Value;
+    }
+
+    public static RuntimeObject CreateFromToken(Token token, RuntimeContext context, int? position = null)
     {
         return token switch
         {
@@ -14,12 +32,19 @@ internal class RuntimeObject
             NumberToken n => CreateFromNumberToken(n),
             WordToken w => CreateFromWordToken(w, context),
             _ => Errors.AlwaysThrow<RuntimeObject>(
-                new SystemError($"{token.Type} cannot be converted to a runtime object."))
+                new SystemError($"{token.Type} cannot be converted to a runtime object."), position: position)
         };
     }
 
     private static RuntimeObject CreateFromWordToken(WordToken token, RuntimeContext context)
     {
+        if (token.ValueAsString == TrueValue)
+            return new BooleanObject(true);
+        if (token.ValueAsString == FalseValue)
+            return new BooleanObject(false);
+        if (token.ValueAsString == NullValue)
+            return new NullObject();
+
         return context.Get(token.ValueAsString);
     }
 
@@ -32,4 +57,8 @@ internal class RuntimeObject
 
         return new IntObject(value);
     }
+
+    private static string TrueValue = "true";
+    private static string FalseValue = "false";
+    private static string NullValue = "null";
 }
