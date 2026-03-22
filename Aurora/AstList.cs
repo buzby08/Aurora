1ab038211f3234aafd906c17e2d61b1959dfc83a
LiamBusby <lbusby685@gmail.com>
2026-03-22 20:02:41 +0000

    Created AST list type

---
+++ Aurora/AstList.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using Aurora.Internals;
+
+namespace Aurora;
+
+internal class AstList : IEnumerable<Ast>
+{
+    public readonly List<Ast> Data = [];
+    public int Count => this.Data.Count;
+
+    public void Add(Ast item)
+    {
+        this.Data.Add(item);
+    }
+    
+    public void Clear()
+    {
+        this.Data.Clear();
+    }
+
+    public RuntimeObject Evaluate(RuntimeContext context)
+    {
+        if (this._evaluatedResult is not null) return this._evaluatedResult;
+        
+        this._evaluatedResult = Evaluator.EvaluateAstList(this, context);
+        return this._evaluatedResult;
+    }
+    
+    public IEnumerator<Ast> GetEnumerator()
+    {
+        return this.Data.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+    private RuntimeObject? _evaluatedResult = null;
+}
