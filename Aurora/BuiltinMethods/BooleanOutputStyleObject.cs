f3b71de298297f447b8eecd1cdf223459aa3a1c3
LiamBusby <lbusby685@gmail.com>
2026-03-22 14:24:12 +0000

    Added helper methods

--- Aurora/BuiltinMethods/BooleanOutputStyleObject.cs
+++ Aurora/BuiltinMethods/BooleanOutputStyleObject.cs
@@ -20,4 +20,51 @@ internal class BooleanOutputStyleObject : RuntimeObject
         Value = value;
         this.Type = Builtins.BooleanOutputStyles;
     }
+
+    public static bool ReadWordOption() => ReadWord("true", "false");
+
+    public static bool ReadYesNo() => ReadWord("yes", "no");
+    
+    public static bool ReadOnOff() => ReadWord("on", "off");
+
+    private static bool ReadImmediateChar(char trueOption, char falseOption)
+    {
+        char? result = null;
+
+        while (result is null || result != trueOption && result != falseOption)
+        {
+            Console.WriteLine($"Please input either {trueOption} or {falseOption}");
+            result = Console.ReadKey().KeyChar;
+        }
+        
+        return result == trueOption;
+    }
+
+    private static bool ReadWord(string trueOption, string falseOption)
+    {
+        string? result = null;
+
+        while (result is null || result != trueOption && result != falseOption)
+        {
+            Console.WriteLine($"Please input either {trueOption} or {falseOption}");
+            result = Console.ReadLine()?.ToLower();
+        }
+        
+        return result == trueOption;
+    }
+
+    public static bool ReadChar(bool immediate)
+    {
+        if (immediate)
+            return ReadImmediateChar('y', 'n');
+        
+        return ReadWord("y", "n");
+    }
+    public static bool ReadBinary(bool immediate)
+    {
+        if (immediate)
+            return ReadImmediateChar('1', '0');
+        
+        return ReadWord("1", "0");
+    }
 }
