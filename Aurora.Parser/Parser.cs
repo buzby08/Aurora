7a7b91168b49db47dd3642ba35beb282c4b52036
LiamBusby <lbusby685@gmail.com>
2026-09-01 11:07:47 +0100

    Fixed unclosed delimiter error

--- Aurora.Parser/Parser.cs
+++ Aurora.Parser/Parser.cs
@@ -422,10 +422,7 @@ public class Parser
     {
         this.Log("Reached block start");
 
-        if (this.State != ParserState.Empty)
-        {
-            this.HandleNewAstStart();
-        }
+        if (this.State != ParserState.Empty) this.HandleNewAstStart();
 
         List<Token> block = [];
 
@@ -433,6 +430,10 @@ public class Parser
         while (bracketDepth > 0)
         {
             Token token = this.GetNextToken();
+            this.Log($"Token: {token}");
+
+            if (token is EofToken)
+                ThrowError(new UnclosedDelimiterError("{ was never closed"), Tokens.First().StartLocation);
 
             if (token is BracketToken { IsCurly: true, IsClosed: true, })
             {
@@ -446,9 +447,10 @@ public class Parser
                 bracketDepth++;
             }
 
-            if (bracketDepth == 0) continue;
+            if (bracketDepth == 0) break;
 
             block.Add(token);
+            this.Log($"Added token to block - count: {block.Count}");
         }
 
         Parser parser = new(block, InternalCallPoint.BlockParsing);
@@ -480,13 +482,14 @@ public class Parser
     {
         if (index >= 0 && index < this.Tokens.Count) return this.Tokens.ElementAtOrDefault(index)!;
 
-        SourceLocation location = new()
+        SourceLocation location = this.Tokens.LastOrDefault()?.EndLocation ?? new SourceLocation
         {
             LineNumber = 0,
             ColumnNumber = 0,
             Offset = 0,
             FilePath = "",
         };
+
         return new EofToken
         {
             StartLocation = location,
