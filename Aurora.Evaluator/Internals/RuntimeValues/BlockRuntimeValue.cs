8f8829e1080efabc3ab4a34244bb1b38c469a949
LiamBusby <lbusby685@gmail.com>
2026-09-16 17:45:07 +0100

    Added toStringValue method

--- Aurora.Evaluator/Internals/RuntimeValues/BlockRuntimeValue.cs
+++ Aurora.Evaluator/Internals/RuntimeValues/BlockRuntimeValue.cs
@@ -12,9 +12,13 @@ public class BlockRuntimeValue(IEnumerable<IEnumerable<Ast>> value) : BaseRuntim
     public Ast[][] GetValue(SourceLocation? location) => base.GetValue<Ast[][]>(location)!;
     public Ast[][] RawValue => (Ast[][])this.Value!;
 
+    public int NumberOfExpressions => this.RawValue.Length;
+
     public static explicit operator BlockRuntimeValue(Ast[][] value) => new(value);
 
     public static RuntimeObject CreateObject(Ast[][] value) => new RuntimeObject(new BlockRuntimeValue(value), Builtins.Block);
 
     private static Ast[][] ConvertToArray(IEnumerable<IEnumerable<Ast>> value) => value.Select(x => x.ToArray()).ToArray();
+
+    public StringRuntimeValue ToStringValue() => (StringRuntimeValue)$"Block<{this.NumberOfExpressions} expression{(this.NumberOfExpressions == 1 ? "" : "s")}>";
 }
